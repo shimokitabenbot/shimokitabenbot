1c328d27b0b47818620aeb046efcadc175b3a987
Gadächtnis <Drossel@JUNO-XV.local>
2014-01-26 15:52:24 +0900

    modify model

--- app/models/word.rb
+++ app/models/word.rb
@@ -5,8 +5,6 @@
 class Word < ActiveRecord::Base
   include ActiveModel::Model
 
-  attr_accessor :id
-
   # validates_xxx_ofを使うと 
   # undefined method `validate' for true:TrueClass
   # が発生するので、項目毎にvalidatesメソッドを呼び出し、条件を書くようにした
