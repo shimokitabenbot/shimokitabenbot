477544f9076191e4112ca82782f18936a85403ff
shimokitabenbot <shimokitaben@yahoo.co.jp>
2014-02-16 19:36:38 +0900

    add model after unit test

--- spec/models/sentence_spec.rb
+++ spec/models/sentence_spec.rb
@@ -3,8 +3,32 @@ require 'spec_helper'
 
 describe Sentence do
   describe 'Invalid Record' do
-    it 'sentence is nil' do
+    it 'sentence and hashtag are nil' do
       expect(Sentence.new).not_to be_valid
+    end
+    
+    it 'sentence is nil' do
+      s = Sentence.new
+      s.hashtag = '#hashtag'
+      expect(s).not_to be_valid
+    end
+
+    it 'hashtag is nil' do
+      s = Sentence.new
+      s.sentence = 'aaa'
+      expect(s).not_to be_valid
+    end
+
+    it 'sentence is exceeded' do
+      s = Sentence.new
+      s.sentence = 'a' * 77
+      expect(s).not_to be_valid
+    end
+
+    it 'hashtag is exceeded' do
+      s = Sentence.new
+      s.hashtag = 'a' * 77
+      expect(s).not_to be_valid
     end
   end
 
